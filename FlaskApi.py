62b2c474795a51d00a3698657f00edb0afa6010d
zh3389 <zhanghao_3389@163.com>
2022-08-14 01:20:13 +0800

    add flask server script

---
+++ FlaskApi.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import os
+import json
+import Bilibili
+
+app = Flask(__name__)
+
+# 有参数
+@app.route("/", methods=['POST'])
+def bilibili():
+    data = request.get_data(as_text=True)
+    jsonObj = json.loads(data)
+    dic = json.loads(jsonObj)
+    Bilibili.freeze_support()
+    Bilibili.main(dic)
+    log_path = "./bilibili.log"
+    with open(log_path, "r") as file:
+        result = file.read()
+    os.remove(log_path)
+    return result
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=9000)
