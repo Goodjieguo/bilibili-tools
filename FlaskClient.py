4ab257ea612a411d24c374d47b67f800a87df7d8
zh3389 <zhanghao_3389@163.com>
2022-08-15 21:26:51 +0800

    feat: 重构flask client

--- FlaskClient.py
+++ FlaskClient.py
@@ -1,23 +1,40 @@
 import json
 import requests
 
-# 更新配置文件
-dic = {"aid_list": ["386644197"],  # aid列表
-    "watch_enable": True,  # 观看开关
-    "like_enable": False,  # 点赞开关
-    "reward_enable": False,  # 投币开关
-    "reward2_enable": False,  # 投币双倍开关
-    "favour_enable": False,  # 收藏开关
-    "combo_enable": False,  # 一键三连开关
-    "share_enable": False,  # 分享开关
-    "danmaku_post_enable": False,  # 弹幕开关
-    "danmaku_content": '哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~',
-    "account": "DedeUserID=125972057;DedeUserID__ckMd5=1b69b079234cf232;SESSDATA=679f430d%2C1675933533%2C65823%2A81;bili_jct=f85e615f38c91d26f50cc4a39f115d9b;sid=15551000;\n",
-    }
+def pull_bv_list(user_dic):
+    url = 'http://1.14.23.34:9000'
+    headers = {'content-type': 'application/json'}
+    h = requests.post(url=url, json=json.dumps(user_dic, ensure_ascii=False), headers=headers)
+    print(h)
+    return_dic = json.loads(h.text)
+    return return_dic
 
-url = 'http://127.0.0.1:9000'
-url = 'http://192.168.31.68:9000'
-headers = {'content-type': 'application/json'}
-h = requests.post(url=url, json=json.dumps(dic, ensure_ascii=False), headers=headers)
-return_dic = json.loads(h.text)
-print(return_dic)
+def push_bv_list(user_dic):
+    url = 'http://1.14.23.34:9000/commit'
+    headers = {'content-type': 'application/json'}
+    h = requests.post(url=url, json=json.dumps(user_dic, ensure_ascii=False), headers=headers)
+    return_dic = json.loads(h.text)
+    return return_dic
+
+if __name__ == '__main__':
+    """
+    switch说明
+    1   观看
+    0   点赞
+    0   投币
+    0   投币双倍
+    0   收藏
+    0   一键三连
+    0   分享
+    0   弹幕
+    """
+    user_dic = {"uid": 700140998,
+                "online": 1,
+                "cookie": "DedeUserID=125972057;DedeUserID__ckMd5=1b69b079234cf232;SESSDATA=679f430d%2C1675933533%2C65823%2A81;bili_jct=f85e615f38c91d26f50cc4a39f115d9b;sid=15551000;\n",
+                "bvlist": "BV1CW4y1h7U2;BV1n14y1b7yw;",
+                "switch": 10000000,
+                }
+    res1 = pull_bv_list(user_dic)  # 下载
+    res2 = push_bv_list(user_dic)  # 上传
+    print(res1)
+    print(res2)
--- autoOpenUrl.py
+++ autoOpenUrl.py
@@ -1,4 +1,3 @@
-
 import time
 import random
 import rpa as r
