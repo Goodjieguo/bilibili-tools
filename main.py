d8105fec7380878092b8a210fdc4908cfb1270bd
zh3389 <zhanghao_3389@163.com>
2022-08-16 14:20:49 +0800

    fix: 修复bug若干 开关权限给到用户等

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ import requests
 import autoOpenUrl
 import LoginBilibili
 import PySimpleGUI as sg
+from PIL import Image, ImageFont, ImageDraw
 
 
 class BilibiliGUI():
     def __init__(self) -> None:
         # Define the window's contents
-        self.layouttop = [[sg.Submit('启动互助', key="-Ok-"), sg.Button('退出互助', key="Quit"), sg.Text(size=(10), key='-OUTPUT-'), ]
+        self.layouttop = [[sg.Submit('启动互助', key="-Ok-"), sg.Button('退出互助', key="Quit"), sg.Text("启动互助后程序会自动隐藏在后台运行.", size=(25), key='-OUTPUT-'), ]
                         ]
         self.layoutr = [[sg.Text("请输入BV号,一行一个(最大支持20)")],
                         [sg.Multiline('BV1CW4y1h7U2\nBV1n14y1b7yw', size=(20,18), key='-bvlist-')]
                         ]
-        self.layoutl = [[sg.Text("观看"), sg.Checkbox('   ', key='-watch-', default=True), sg.Text("点赞"), sg.Checkbox('', key='-like-')],
+        self.layoutl = [[sg.Text("观看"), sg.Checkbox('   ', key='-watch-', default=True), sg.Text("点赞"), sg.Checkbox('', key='-like-', default=True)],
+                        [sg.Text("收藏"), sg.Checkbox('   ', key='-favour-', default=True), sg.Text("分享"), sg.Checkbox('', key='-share-', default=True)],
                         [sg.Text("投币"), sg.Checkbox('   ', key='-reward-'), sg.Text("双倍"), sg.Checkbox('', key='-reward2-')],
-                        [sg.Text("收藏"), sg.Checkbox('   ', key='-favour-'), sg.Text("分享"), sg.Checkbox('', key='-share-')],
                         [sg.Text("弹幕"), sg.Checkbox('   ', key='-danmaku_post-'), sg.Text("三连"), sg.Checkbox('', key='-combo-')],
-                        [sg.Text("弹幕内容(该功能敬请期待):")],
+                        [sg.Text("弹幕内容:")],
                         [sg.Multiline('哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~', size=(20,10), key='-danmaku_content-')]
                         ]
         self.layout = [[sg.Col(self.layouttop)], [sg.Col(self.layoutl), sg.Col(self.layoutr)]]
+        pass
 
     # 获取gui输入的bilibili参数
     def transforGuiInput(self, cookie):
@@ -75,6 +77,8 @@ class BilibiliGUI():
         # 将所有bv号提取到一个列表当中
         online_bv_list = []
         for li in online_return_dic[bvlist_name]:
+            if li[0] == None:
+                continue
             online_bv_list.extend([x for x in li[0].split(";") if x!= ""])  # 删除空bv号并添加到bvlist里面
         # 添加本地提交的BV号
         online_bv_list.extend(self.values['-bvlist-'].split("\n"))
@@ -101,39 +105,41 @@ class BilibiliGUI():
         # 更新配置文件
         for vip_aid in vip_aid_list:
             dic = {"aid_list": [vip_aid],  # aid列表
-                    "watch_enable": True,  # 观看开关
-                    "like_enable": True,  # 点赞开关
-                    "reward_enable": False,  # 投币开关
-                    "reward2_enable": False,  # 投币双倍开关
-                    "favour_enable": True,  # 收藏开关
-                    "combo_enable": True,  # 一键三连开关
-                    "share_enable": True,  # 分享开关
-                    "danmaku_post_enable": True,  # 弹幕开关
-                    "danmaku_content": '哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~", "哔哩哔哩 (゜-゜)つロ 干杯~',
+                    "watch_enable": self.values['-watch-'],  # 观看开关
+                    "like_enable": self.values['-like-'],  # 点赞开关
+                    "reward_enable": self.values['-reward-'],  # 投币开关
+                    "reward2_enable": self.values['-reward2-'],  # 投币双倍开关
+                    "favour_enable": self.values['-favour-'],  # 收藏开关
+                    "combo_enable": self.values['-combo-'],  # 一键三连开关
+                    "share_enable": self.values['-share-'],  # 分享开关
+                    "danmaku_post_enable": self.values['-danmaku_post-'],  # 弹幕开关
+                    "danmaku_content": [self.values['-danmaku_content-']],
                     "account": f"{cookie}\n",
                     }
             bilibili.main(dic)
             time.sleep(3)
 
-    def run(self, vis_chrome=False):
+    def run(self, vis_chrome=True):
         cookie, credential = LoginBilibili.login()
         # cookie = "DedeUserID=700140998;DedeUserID__ckMd5=4b0ff4550adde510;SESSDATA=e606e066%2C1676111088%2C7f2fe%2A81;bili_jct=0f585b8281c89043d082f7558ffaa44a;sid=15551000;\n"
         # Create the window
         window = sg.Window('其它酱', self.layout)
         while True:
             self.event, self.values = window.Read(timeout=100)
-            if len(cookie) < 2 or cookie == None:
-                window['-OUTPUT-'].update("请重新登录.")
-                return
             if self.event == "-Ok-":  # 互助开启
-                sg.popup_ok("点击OK 程序后台将自动互助\n无法点击程序是正常情况, 挂机即可\n", title="互助提醒")
+                sg.popup_auto_close(f"程序后台将自动互助\n无法点击程序是正常情况, 挂机即可", title="互助提醒")
+                window.hide()
                 upload_dic = self.transforGuiInput(cookie)  # 转换输入数据为上传dict
                 online_return_dic = pull_bv_list(upload_dic)  # 拉取数据库数据 获取bvlist
-
-                if online_return_dic["expired"]:
-                    window['-OUTPUT-'].update(f"会员已过期: {online_return_dic['duedate']}")
-                else:
-                    window['-OUTPUT-'].update(f"会员互助已启动: {online_return_dic['duedate']}")
+                push_bv_list(upload_dic)
+                # 打开一张图片说明
+                img = Image.new("RGB", size=(1000, 500),color=(255,255,255))
+                draw = ImageDraw.Draw(img)
+                draw.text((0,0),
+                        f"其他酱正在后台运行, 无需关闭电脑即可互助.(此图可关闭)\n会员到期时间:{online_return_dic['duedate']}\n\n售后 更新 升级请加QQ群 936622419\n\n软件前期为了增加大家的互动, 对会员用户默认开启 观看 点赞 收藏 分享 弹幕 \n实时显示互助效果正在开发中, 请关注下一个版本.",
+                        (255,0,0),
+                        font = ImageFont.truetype('FZCCHJW.ttf', 25))
+                img.show()
                 all_url_list, all_aid_list, vip_url_list, vip_aid_list = self.get_url_list_aid_list(online_return_dic)
                 self.visit_vip_aid(cookie, vip_aid_list)  # 访问会员aid
                 autoOpenUrl.main({"url_list": all_url_list, "vis_chrome": vis_chrome})
@@ -158,5 +164,6 @@ def push_bv_list(user_dic):
     return return_dic
 
 if __name__ == "__main__":
+    freeze_support()
     gui = BilibiliGUI()
     gui.run()
